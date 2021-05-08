abb02663ee72cc4206ccc4fc502b6ff46eea7131
ollihansen90 <50058898+ollihansen90@users.noreply.github.com>
2021-05-08 15:14:06 +0200

    Update WeavedMLP.py

--- WeavedMLP.py
+++ WeavedMLP.py
@@ -6,6 +6,8 @@ from GroupedLinear import GroupedLinear
 class WeavedMLP(nn.Module):
     def __init__(self, dim_in, dim_out, hidden_dims, n_groups=2, dropout=0.):
         super(WeavedMLP, self).__init__()
+        if isinstance(hidden_dims, int):
+            hidden_dims = [hidden_dims]
         self.depth = len(hidden_dims)-1
         self.n_groups = n_groups
 
@@ -25,15 +27,22 @@ class WeavedMLP(nn.Module):
         self.activation = nn.ReLU()
         self.dropout = nn.Dropout(dropout)
         
-   def forward(self, x):
-        b, n = x.shape
+    def forward(self, x):
+        if len(x.shape)==1:
+            x = x.unsqueeze(0).unsqueeze(0)
+        elif len(x.shape)==2:
+            x = x.unsqueeze(1)
+        b, c, n = x.shape
+        x = x.flatten(start_dim=0, end_dim=1)
+
         for layer in self.layers:
             x = layer(x)
             x = self.activation(x)
             x = self.dropout(x)
-
+            n = int(x.shape[-2]/self.n_groups)
+            
             # Verweben
-            x = x.reshape((b,n/self.groups, self.groups))
+            x = x.reshape((b, c, n, self.n_groups))
             x = x.transpose(-1,-2)
             x = x.flatten(start_dim=-2, end_dim=-1)
-        return self.activation(self.out(x))
+        return self.activation(self.outlayer(x))
